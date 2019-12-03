365367ee54c8f50ebc5b1fef1ccdaf04e9eff178
SRGNation <32860440+SRGNation@users.noreply.github.com>
2019-12-02 16:22:07 -0800

    Mii's work now.

--- php/inc/footer.php
+++ php/inc/footer.php
@@ -2,7 +2,7 @@
             <div id="footer">
                 <div id="footer-inner">
                     <div class="link-container">
-                        <p><a href="https://github.com/PF2M/ProjectULTIMA">Github</a></p>
+                        <p><a href="https://github.com/SRGNation/ProjectULTIMA">Github</a></p>
                         <p><a href="https://srgnation.itch.io/freshmans-adventure/">A Freshman's Adventure</a></p>
                         <p><a href="/rules">Site Rules</a></p>
                         <p id="copyright"><a href="https://nintendo.com/">Project ULTIMA is a nonprofit fan project based on assets from Nintendo and Hatena, and is not affiliated with either company.</a></p>
