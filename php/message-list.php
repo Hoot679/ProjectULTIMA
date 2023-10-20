71113ee240e33a36dec010039fad6bd433136ef0
Aubri <32860440+SRGNation@users.noreply.github.com>
2023-10-20 09:14:34 -0700

    Fixed message list, made it look slightly better

--- php/message-list.php
+++ php/message-list.php
@@ -33,19 +33,39 @@ $row = initUser($_SESSION['username'], true);
       					$uresult = $stmt->get_result();
       					$urow = $uresult->fetch_assoc();
 
-                $stmt = $db->prepare("SELECT id, body, created_at, created_by, seen FROM messages WHERE conversation = ? AND status = 0 ORDER BY id DESC LIMIT 1");
-                $stmt->bind_param('i', $row['id']);
-                $stmt->execute();
-                $mresult = $stmt->get_result();
-                $mrow = $mresult->fetch_assoc();
-
-      					echo '<div class="news-list-content trigger'.($mrow['seen'] === 0 & $mrow['created_by'] !== $_SESSION['id'] ? ' notify' : '');
-                        echo '" id="' . $row['id'] . '" data-href="/messages/'.htmlspecialchars($urow['username']).'" tabindex="0"><a href="/users/' . htmlspecialchars($urow['username']);
-                        echo '" class="icon-container';
-                        if($urow['level'] > 0) {
-                            echo ' official-user';
+                        $stmt = $db->prepare("SELECT id, body, created_at, created_by, seen FROM messages WHERE conversation = ? AND status = 0 ORDER BY id DESC LIMIT 1");
+                        $stmt->bind_param('i', $row['id']);
+                        $stmt->execute();
+                        $mresult = $stmt->get_result();
+                        $mrow = $mresult->fetch_assoc();
+                        
+                        if($mresult->num_rows > 0) {
+                            if(strlen($mrow['body']) > 60) {
+                                $body = htmlspecialchars(mb_substr($mrow['body'],0,57).'...');
+                            } else {
+                                $body = htmlspecialchars($mrow['body']);
+                            }
+                        
+                            if($mrow['created_by'] == $_SESSION['id'] && $mrow['seen'] == 0) {
+                                $body = '<em>Sent</em> - '.$body;
+                            } else if($mrow['created_by'] == $_SESSION['id'] && $mrow['seen'] == 1) {
+                                $body = '<em>Seen</em> - '.$body;
+                            }
                         }
-                        echo '"><img class="icon" src="'.getAvatar($urow['avatar'], $urow['has_mh']).'"></a><div class="body"><a href="/users/' . htmlspecialchars($urow['username']) . '" class="nick-name">' . htmlspecialchars($urow['nickname']) . '</a>'.($mresult->num_rows !== 0 ? '<p>'.htmlspecialchars($mrow['body']).'</p>' : '<p>You have not exchanged messages with this user yet.</p>');
+                        else
+                        {
+                            $body = '<em>You have not exchanged messages with this user yet.</em>';
+                        }
+
+      					if($mresult->num_rows !== 0) {
+      					    echo '<div class="news-list-content trigger'.($mrow['seen'] === 0 & $mrow['created_by'] !== $_SESSION['id'] ? ' notify' : '');
+      					}
+      					else
+      					{
+      					    echo '<div class="news-list-content trigger';
+      					}
+                        echo '" id="' . $row['id'] . '" data-href="/messages/'.htmlspecialchars($urow['username']).'" tabindex="0"><a href="/users/' . htmlspecialchars($urow['username']);
+                        echo '" class="icon-container"><img class="icon" src="'.getAvatar($urow['avatar'], $urow['has_mh']).'"></a><div class="body"><a href="/users/' . htmlspecialchars($urow['username']) . '" class="nick-name'.($urow['level'] > 0 ? ' official-user' : '').'">' . htmlspecialchars($urow['nickname']) . '</a>'.($mresult->num_rows !== 0 ? '<p>'.$body.'</p>' : '<p><em>You have not exchanged messages with this user yet.</em></p>');
                         if($mresult->num_rows !== 0) {
                           echo '<span class="timestamp">' . getTimestamp($mrow['created_at']) . '</span>';
                         }
@@ -53,9 +73,6 @@ $row = initUser($_SESSION['username'], true);
       				}
       			}
       		}
-          $stmt = $db->prepare("UPDATE messages SET seen = 1 WHERE conversation IN (SELECT id FROM conversations WHERE source = ? OR target = ?) AND created_by != ?");
-          $stmt->bind_param('iii', $_SESSION['id'], $_SESSION['id'], $_SESSION['id']);
-          $stmt->execute();
       		?>
         </div>
     </div>
